ae663435190d701b109027ef3e031f0a60ca92e7
AGMODDER <81137652+AGMODDER@users.noreply.github.com>
2022-05-02 15:34:49 -0300

    up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/device.mk \
-    $(LOCAL_DIR)/lineage_sofiar.mk
+    $(LOCAL_DIR)/superior_sofiar.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_sofiar-eng \
-    lineage_sofiar-user \
-    lineage_sofiar-userdebug
+    superior_sofiar-eng \
+    superior_sofiar-user \
+    superior_sofiar-userdebug
