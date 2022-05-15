eedb1b483109a07129c7696c843233fcc3927cac
AGMODDER <81137652+AGMODDER@users.noreply.github.com>
2022-05-15 14:14:29 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/device.mk \
-    $(LOCAL_DIR)/crdroid_sofiar.mk
+    $(LOCAL_DIR)/lineage_sofiar.mk
 
 COMMON_LUNCH_CHOICES := \
-    crdroid_sofiar-eng \
-    crdroid_sofiar-user \
-    crdroid_sofiar-userdebug
+    lineage_sofiar-eng \
+    lineage_sofiar-user \
+    lineage_sofiar-userdebug
