8ad9c87a744504e28497dfb27e468eaa7ea63318
AGMODDER <81137652+AGMODDER@users.noreply.github.com>
2022-06-21 10:13:54 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/device.mk \
-    $(LOCAL_DIR)/lineage_sofiar.mk
+    $(LOCAL_DIR)/palladium_sofiar.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_sofiar-eng \
-    lineage_sofiar-user \
-    lineage_sofiar-userdebug
+    palladium_sofiar-eng \
+    palladium_sofiar-user \
+    palladium_sofiar-userdebug
