0e6d08dc385304342a36a4152e81f25e8d89779e
AGMODDER <81137652+AGMODDER@users.noreply.github.com>
2022-08-15 17:30:03 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/device.mk \
-    $(LOCAL_DIR)/lineage_sofiar.mk
+    $(LOCAL_DIR)/syberia_sofiar.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_sofiar-eng \
-    lineage_sofiar-user \
-    lineage_sofiar-userdebug
+    syberia_sofiar-eng \
+    syberia_sofiar-user \
+    syberia_sofiar-userdebug
